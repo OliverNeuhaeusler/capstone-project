6c686d2192d5ca9efc999dc219183c014615828b
Oliver Neuhäusler <oliver-neuhaeusler@gmx.net>
2021-06-04 15:02:47 +0200

    put app.js in edit index.js and delete everything we dont nedd

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { Switch, Route } from 'react-router-dom';
+import styled from 'styled-components/macro';
+import BurgerMenu from './BurgerMenu.js';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header>
+        <h1>Mittelalter-Märkte</h1>
+      </Header>
+      <main>
+        <BurgerMenu />
+        <Switch>
+          <Route exact path="/">
+            Home
+          </Route>
+          <Route path="/markt">Märkte</Route>
+          <Route path="/favorites">Favoriten</Route>
+          <Route path="/createmarkt">Markt erstellen</Route>
+          <Route path="/profil">Profil</Route>
+          <Route path="/contact">Kontakt</Route>
+          <Route path="/impressum">Impressum</Route>
+        </Switch>
+      </main>
     </div>
   );
 }
 
 export default App;
+
+const Header = styled.header`
+  display: flex;
+
+  h1 {
+    display: flex;
+    background: hsl(37, 48%, 38%);
+    border: groove 0.5rem goldenrod;
+    border-radius: 0.8rem;
+    color: hsl(37, 19%, 70%);
+    justify-content: center;
+    left: 0;
+    margin: 0 auto;
+    padding: 1rem;
+    position: fixed;
+    top: 0;
+    width: 100%;
+  }
+`;
---
+++ client/src/Globalstyle.js
@@ -0,0 +1,12 @@
+import { createGlobalStyle } from 'styled-components';
+
+export default createGlobalStyle`
+* {
+    box-sizing: border-box;
+    line-height: 1.5;
+    font-family: sans-serif;
+}
+body {
+    background: hsl(37, 19%, 70%);
+}
+`;
